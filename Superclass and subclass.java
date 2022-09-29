bf4152d2cf9c6f89050b0980d74cbe48a883d0c1
Cecilia AP <89456756+names-cecilia@users.noreply.github.com>
2022-09-29 14:54:22 +0700

    Update Superclass and subclass.java

--- Superclass and subclass.java
+++ Superclass and subclass.java
@@ -1,3 +1,4 @@
+//superclass
 public class Mahasiswa {
     //mendeklarasikan variabel beserta datatype
     public String Nama;
@@ -18,6 +19,32 @@ public class Mahasiswa {
     public String getNIM(){
         return NIM;
     }
+    
+    //subclass pertama
+        public class Mahasiswa_S1 extends Mahasiswa{
+    public Mahasiswa_S1(String Nama, String NIM, float IPK){
+        super(Nama, NIM, IPK);//variabel dari superclass
+    
+    //menghitung ipk apakah cumlaude atau tidak
+    //operasi disimpan dalam method isCumlaude
+    public void isCumlaude(){
+        if (IPK > 3.50) {
+            System.out.println("Mahasiswa Cumlaude");
+        } else {
+            System.out.println("Tidak Cumlaude");
+        }
+    }
+    
+    //menampilkan data mahasiswa
+    public void dataMahasiswa(){
+        System.out.println("\nData Mahasiswa S1");
+        System.out.println("Nama : " + getNama());
+        System.out.println("NIM : " + getNIM());
+        System.out.println("IPK : " + getIPK());
+    }
+}
+
+    }
     public float getIPK(){
         return IPK;
     }
@@ -34,3 +61,78 @@ public class Mahasiswa {
     }
     
 }
+
+
+//subclass kedua
+public class Mahasiswa_S3 extends Mahasiswa{
+    public Mahasiswa_S3(String Nama, String NIM, float IPK){
+        super(Nama, NIM, IPK); //variabel dari superclass
+    }
+    
+    //menghitung ipk apakah cumlaude atau tidak
+    //disimpan dalam public method
+    public void isCumlaude(){
+        if (IPK > 3.90) {
+            System.out.println("Mahasiswa Cumlaude");
+        } else {
+            System.out.println("Tidak Cumlaude");
+        }
+    }
+    
+    //menampilkan data mahasiswa
+    public void dataMahasiswa(){
+        System.out.println("\nData Mahasiswa S3");
+        System.out.println("Nama : " + getNama());
+        System.out.println("NIM : " + getNIM());
+        System.out.println("IPK : " + getIPK());
+    }
+}
+
+
+
+//subclass ketiga
+public class Mahasiswa_S3 extends Mahasiswa{
+    public Mahasiswa_S3(String Nama, String NIM, float IPK){
+        super(Nama, NIM, IPK); //variabel dari superclass
+    }
+    
+    //menghitung ipk apakah cumlaude atau tidak
+    //disimpan dalam public method
+    public void isCumlaude(){
+        if (IPK > 3.90) {
+            System.out.println("Mahasiswa Cumlaude");
+        } else {
+            System.out.println("Tidak Cumlaude");
+        }
+    }
+    
+    //menampilkan data mahasiswa
+    public void dataMahasiswa(){
+        System.out.println("\nData Mahasiswa S3");
+        System.out.println("Nama : " + getNama());
+        System.out.println("NIM : " + getNIM());
+        System.out.println("IPK : " + getIPK());
+    }
+}
+
+//subclass keempat
+public class UjiMahasiswa {
+    public static void main(String[] args) {
+        //mengisi data/parameter objek setiap kelas
+        
+        Mahasiswa_S1 a = new Mahasiswa_S1("Andrea", "0903", (float) 3.00);
+        a.dataMahasiswa(); //memanggil dataMahasiswa untuk ditampilkan
+        a.isCumlaude(); //memanggil operasi isCumlaude
+        
+        Mahasiswa_S2 b = new Mahasiswa_S2("Surya", "0802", (float) 3.75);
+        b.dataMahasiswa(); //memanggil data Mahasiswa untuk ditampilkan
+        b.isCumlaude();//memanggil operasi isCumlaude
+        
+        Mahasiswa_S3 c = new Mahasiswa_S3("Nala", "0701", (float) 4.00);
+        c.dataMahasiswa(); //memanggil data Mahasiswa untuk ditampilkan
+        c.isCumlaude(); //memanggil operasi isCumlaude
+        System.out.println("--------------------");
+    }
+}
+
+
