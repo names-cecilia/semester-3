928078b7809d2aa4f0751fc666c0a514b15b419f
Cecilia AP <89456756+names-cecilia@users.noreply.github.com>
2022-10-01 17:32:32 +0700

    Add files via upload

---
+++ OutputVariable.java
@@ -0,0 +1,11 @@
+public class OutputVariable {
+  public static void main(String[] args) {
+      int value = 10; /* menyatakan nilai 'value' dan menentukan tipe data */
+        char x; /* directory karakter x yang didefinisikan sebagai char */
+        x = 'A'; /* variabel x yang di definisikan sebagai 'A' */
+        
+        System.out.println(value);
+        System.out.println("The value of x = " + x);
+}
+  
+}
