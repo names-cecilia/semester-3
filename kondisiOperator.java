af363abe99bbb3a1421607c4ea5c6e387952ee61
Cecilia AP <89456756+names-cecilia@users.noreply.github.com>
2022-10-01 17:34:21 +0700

    Add files via upload

---
+++ kondisiOperator.java
@@ -0,0 +1,12 @@
+public class kondisiOperator {
+    public static void main(String[] args) {
+        int score = 0;
+        char answer = 'a';
+        
+        score = (answer == 'a')? 10 : 0;
+        /*checking the condition of 'answer' variable
+        if answer defined as 'a', then the score value is 10
+        else, the score is 0.*/
+        System.out.println("Score = " + score);
+}
+}
