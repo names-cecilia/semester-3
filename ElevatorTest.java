2fc00c3b43d7469db57a3764e8481fac7fb14cf9
Cecilia AP <89456756+names-cecilia@users.noreply.github.com>
2022-10-03 11:58:34 +0700

    Add files via upload

---
+++ ElevatorTest.java
@@ -0,0 +1,18 @@
+public class ElevatorTest {
+    public static void main(String[] args) {
+        Elevator e = new Elevator();
+        //calling all function and do all steps below
+        e.bukaPintu();
+        e.tutupPintu();
+        e.turun();
+        e.naik();
+        e.naik();
+        e.naik();
+        e.bukaPintu();
+        e.tutupPintu();
+        e.turun();
+        e.bukaPintu();
+        e.turun();
+        e.bukaPintu();           
+    }
+}
